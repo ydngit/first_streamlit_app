1a0837a435765063411c6def9d0540b3141184fb
Durganand Yedlapati <121146849+ydngit@users.noreply.github.com>
2022-12-20 23:45:31 -0500

    Update streamlit_app.py
    updated heading and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents new Healthy Diner is Great')
+streamlit.title('My Parents new Healthy Diner')
+
+streamlit.header('Breakfat Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
