09a92a211910c5233d420ef1dbb422ab415ea9ab
Durganand Yedlapati <121146849+ydngit@users.noreply.github.com>
2022-12-20 23:48:52 -0500

    Update streamlit_app.py
    Added icons and another header for Smoothie.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
-
 streamlit.header('Breakfat Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
