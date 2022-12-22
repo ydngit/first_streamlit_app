4deea1d91b82a27b54c21064bb0f097080af6e37
Durganand Yedlapati <121146849+ydngit@users.noreply.github.com>
2022-12-22 18:50:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
